f94fadd015a0d4166f51ab56610dd607c280f3ce
nnottp33 <6110210130@psu.ac.th>
2022-06-07 11:43:31 +0700

    first commit

---
+++ app/Views/main/admin/dashboard.php
@@ -0,0 +1,15 @@
+ <?= $this->extend('template/layout') ?>
+
+ <?php $this->section('content'); ?>
+ <div class="page-wrapper">
+
+     <div class="page-breadcrumb">
+
+     </div>
+
+     <div class="container-fluid">
+
+
+     </div>
+ </div>
+ <?= $this->endSection();
