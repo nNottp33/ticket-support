cd7b877ce935452fc78956df1578240ff6d1b392
nnottp <6110210130@psu.ac.th>
2022-06-21 21:39:30 +0700

    edit ux/ui dashboard

--- app/Views/main/admin/dashboard.php
+++ app/Views/main/admin/dashboard.php
@@ -84,7 +84,7 @@
          </div>
 
          <div class="row">
-             <div class="col-md-6 col-lg-8">
+             <div class="col-md-12 col-lg-6">
                  <div class="card">
                      <div class="card-body">
                          <div class="d-flex align-items-center mb-4">
